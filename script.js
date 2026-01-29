aa748be0c45cbc56db2879417d689450fa5e0b2f
Venkat Kesav Singh <venkatkesavsingh@gmail.com>
2026-01-30 02:03:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/1
  * FIREBASE CONFIG
  ********************************/
 const firebaseConfig = {
-  apiKey: "AIzaSyCFE2GuML1GCaWPoGHmoiFfKX_WW55kktY",
-  authDomain: "quiz-room-08.firebaseapp.com",
-  databaseURL: "https://quiz-room-08-default-rtdb.firebaseio.com",
-  projectId: "quiz-room-08",
-  storageBucket: "quiz-room-08.firebasestorage.app",
-  messagingSenderId: "62287969743",
-  appId: "1:62287969743:web:47bf77b38dde3fb7b626ba"
+  apiKey: "AIzaSyBtjeH6cqx4tuoaXqHc_PGa2-wpt4V5g28",
+  authDomain: "quiz-room-05.firebaseapp.com",
+  databaseURL: "https://quiz-room-05-default-rtdb.firebaseio.com",
+  projectId: "quiz-room-05",
+  storageBucket: "quiz-room-05.firebasestorage.app",
+  messagingSenderId: "894494206201",
+  appId: "1:894494206201:web:351e121fdac5ffc0dada28"
 };
 
 const app = initializeApp(firebaseConfig);
